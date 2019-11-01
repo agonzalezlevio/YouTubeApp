22b26ec16c10747c9d53e76e7b5f9a02bfb409ce
axlfrost <ale.gonzalezlevio@gmail.com>
2019-11-01 18:20:47 -0300

    Se crea el pipe para filtrar vídeos de origen "unsafe"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule,
